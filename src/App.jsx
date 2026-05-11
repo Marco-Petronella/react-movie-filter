5445836b35e2e2be88ad74c5aadc5acd570e1b47
Marco-Petronella <marco.petronella85@gmail.com>
2026-05-11 19:42:58 +0200

    add title research

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,20 @@ function App() {
     setFilms(filmsList)
   }, [])
 
-  function handleFilter() {
-    const selectedFilter = document.getElementById("filter").value;
+  function handleGenreFilter() {
+    const selectedFilter = document.getElementById("filterGenre").value;
     if (selectedFilter != "ALL") { 
       const filteredFilms = originalList.filter((film) => film.genre == selectedFilter)
+      setFilms(filteredFilms) 
+    }
+      else {
+      setFilms(originalList)
+      }
+  }
+    function handleTitleFilter() {
+    const selectedFilter = document.getElementById("filterTitle").value;
+    if (selectedFilter != "ALL") { 
+      const filteredFilms = originalList.filter((film) => film.title.toLowerCase().includes(selectedFilter.toLowerCase()))
       setFilms(filteredFilms) 
     }
       else {
@@ -23,13 +33,15 @@ function App() {
   
   return (
     <>
-    <span className="m-2">filtra per: </span><select onChange={handleFilter} className="m-2" name="" id="filter">
+    <span className="m-2">filtra per: </span><select onChange={handleGenreFilter} className="m-2" name="" id="filterGenre">
       <option value="ALL">Tutti i film</option>
       <option value="Fantascienza">Fantascienza</option>
       <option value="Thriller">Thriller</option>
       <option value="Romantico">Romantico</option>
       <option value="Azione">Azione</option>
     </select>
+    <p className="m-2">Ricerca film per titolo: <input onChange={handleTitleFilter} className="m-2" name="" id="filterTitle"></input></p>
+
     <ul>
     {films.map(film => (
       <li key={film.title}>{film.title} , {film.genre}</li>
